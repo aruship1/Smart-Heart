11a568d90098215c103aee02312fd58f37dc8c3d
marissashey <marissashey1@gmail.com>
2017-08-18 16:26:44 -0400

    New Smart Heart file

---
+++ Smart Heart copy/Smart Heart/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Smart Heart
+//
+//  Created by GWC Student on 8/16/17.
+//  Copyright © 2017 Smarties. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+//    @IBOutlet weak var inputName: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //Handle the text field's user input through delegate callbacks
+//        inputName.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
